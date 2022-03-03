0956845043155c6024016396402d4492fd6d3de0
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-03-04 00:15:10 +0900

    22/03/04 계산기 UI 그리기

--- app/src/main/java/com/kkuber/myapp_04_calculator/MainActivity.kt
+++ app/src/main/java/com/kkuber/myapp_04_calculator/MainActivity.kt
@@ -2,10 +2,15 @@ package com.kkuber.myapp_04_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v : View) {
+
     }
 }
