bdc0fdb10c78e7c30a011a26d7c07823d26f731d
jjyu <79048655+jjyu9312@users.noreply.github.com>
2022-03-05 00:55:12 +0900

    22/03/04 계산기 UI 그리기

--- app/src/main/java/com/kkuber/myapp_04_calculator/MainActivity.kt
+++ app/src/main/java/com/kkuber/myapp_04_calculator/MainActivity.kt
@@ -11,6 +11,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buttonClicked(v : View) {
+        when (v.id) {
+            R.id.button0 -> numberButtonClicked("0")
+            R.id.button1 -> numberButtonClicked("1")
+            R.id.button2 -> numberButtonClicked("2")
+            R.id.button3 -> numberButtonClicked("3")
+            R.id.button4 -> numberButtonClicked("4")
+            R.id.button5 -> numberButtonClicked("5")
+            R.id.button6 -> numberButtonClicked("6")
+            R.id.button7 -> numberButtonClicked("7")
+            R.id.button8 -> numberButtonClicked("8")
+            R.id.button9 -> numberButtonClicked("9")
+            R.id.buttonPlus -> numberButtonClicked("")
+            R.id.buttonMultiple -> numberButtonClicked("9")
+            R.id.buttonDivision -> numberButtonClicked("9")
+            R.id.buttonMinus -> numberButtonClicked("9")
+        }
+    }
+
+    private fun numberButtonClicked(number : String) {
+
+    }
+
+    private fun
+
+    fun resultButtonClicked(v : View) {
+
+    }
+
+    fun clearButtonClicked(v : View) {
+
+    }
+
+    fun uttonClicked(v : View) {
 
     }
 }
